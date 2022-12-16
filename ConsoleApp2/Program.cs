6f4365ec2a199597f4f5ecd91831ace044846eaa
РомаН <119739400+romausnul@users.noreply.github.com>
2022-12-16 23:01:01 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static double GetParallelResistance(double r1, double r2)
+        {
+            if ((r1 <= 0) || (r2 <= 0))
+            {
+                Console.WriteLine("Введены неверные значения сопротивлений.");
+                Environment.Exit(1);
+            }
+            return r1 * r2 / (r1 + r2);
+        }
+
+        static void Main()
+        {
+            double r1 = 0, r2 = 0;
+            try
+            {
+                Console.WriteLine("Введите значение первого сопротивления: ");
+                r1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите пж второго сопротивления: ");
+                r2 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный ввод, ты что совсем уже.");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Сопротивление при параллельном соединении: {0}", GetParallelResistance(r1, r2));
+        }
+    }
+}
